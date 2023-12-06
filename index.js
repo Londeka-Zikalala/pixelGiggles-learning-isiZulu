e9da0d146998ffd2bfc49334e9057b93799a3102
Thembakazi <thembakazingamlana25@gmail.com>
2023-12-06 19:04:05 +0200

    beginner level stage 1

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ import bodyParser from 'body-parser';
 import pgPromise from 'pg-promise';
 import flash from 'express-flash';
 import session from 'express-session';
+import learningIsizulu from './services/learningIsizulu.js'
 
 const pgp = pgPromise();
 
+
+
 const connectionString = process.env.DATABASE_URL || 'postgres://learning_isizulu_db_user:YvwWEV8OVJ2kQFSCnhY65Y5olSPwqWcP@dpg-clnelm5e89qs739eb6l0-a.oregon-postgres.render.com/learning_isizulu_db?ssl=true';
 const db = pgp(connectionString);
-
+const learningIsizuluRoute = learningIsizulu(db)
 const app = express();
 
 app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: true }));
@@ -40,16 +43,52 @@ app.get('/', async (req, res) => {
   res.render('home', {});
 });
 
-app.get('/next-page', (req, res) => {
+app.get('/next-page', async(req, res) => {
+ 
   // Handle the logic for the next page
   res.render('Screen', {}); 
 });
 
+app.post('/beginer', async(req, res)=>{
+  let username = req.body.username
+  await learningIsizuluRoute.insertPlayer(username)
+    res.redirect(`beginer`)
+})
 //route for the begginner level
-app.get('/beginer',(req, res)=> {
+app.get('/beginer',async (req, res)=> {
+const getBegginnerLevel =  await learningIsizuluRoute.getBeginnerLevel()
+// Using dummy values here since it is breaking 
+const username = 'Dr Smit'
+const level = 'beginer'
+const userId = 1
+let stage1;
+let stage2 ; 
+let stage3 ;
 
+for(var i = 0 ; i < getBegginnerLevel ; i++){
+ let stages = getBegginnerLevel[i]
+  if(stages === 'Sawubona'){
+    stage1 = stages
+  let updateUserProgress =  await learningIsizuluRoute.updateUserProgress(userId,username, stage1, level)
+  } else if(stages === 'Unjani'){
+    stage2 = stages
+    let updateUserProgress =  await learningIsizuluRoute.updateUserProgress(userId,username, stage2, level)
 
-  res.render('beginer',{})
+  } else if(stages === 'Ngiyaphila'){
+    stage3 = stages
+    let updateUserProgress =  await learningIsizuluRoute.updateUserProgress(userId,username, stage1, level)
+    console.log(updateUserProgress)
+  }
+
+}
+
+console.log( getBegginnerLevel)
+  res.render('beginer',{
+    getBegginnerLevel,
+    stage1,
+    stage2, 
+    stage3
+  })
 })
 
 
@@ -58,37 +97,11 @@ app.get('/beginer',(req, res)=> {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log('App started at port', PORT);
 });
+
+
+///Oh girl I see you have no connection now? 
